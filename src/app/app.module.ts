81d8068c6fa68233ae9aea57c5bcdda31d3fb87a
RenanSouza <renansouza0122@gmail.com>
2023-11-29 17:46:01 -0300

    card form added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { DetailsFormComponent } from './components/details-form/details-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailsFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/details-form/details-form.component.ts
@@ -0,0 +1,61 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-details-form',
+  templateUrl: './details-form.component.html',
+  styleUrls: ['./details-form.component.scss']
+})
+export class DetailsFormComponent {
+   
+  registerForm = this.fb.group({
+      name:['', Validators.required],
+      number:['',[Validators.required,this.cardNumberValidation]],
+      month:['', [Validators.required, Validators.pattern('^(0[1-9]|1[0-2])$')]],
+      year:['', [Validators.required ,this.yearValidation]],
+      cvc:['', [Validators.required,this.cvcValidation]],
+  });
+
+  isSubmitted!:boolean;
+  constructor(private fb: FormBuilder){}
+  
+  submit():void{
+    console.log("submitted",this.registerForm.value,this.registerForm.invalid);
+
+    this.isSubmitted = true;
+  }
+
+  cardNumberValidation(control: { value: any; }) {
+    const input = control.value;
+  
+
+    if (!/^(\d{4}\s){3}\d{4}$/.test(input)) {
+      return { 'invalidPattern': true };
+    }
+  
+    return null;
+  }
+  yearValidation(control: { value: any; }){
+    const year = control.value;
+
+
+    if (!/^\d{2}$/.test(year)) {
+      return { 'invalidTwoDigitYear': true };
+    }
+
+    return null;
+  }
+
+  cvcValidation(control: { value: any; }){
+    const cvcValue = control.value;
+
+
+    if (!/^\d{3}$/.test(cvcValue)) {
+      return { 'invalidThreeDigitCvc': true };
+    }
+    return null;
+  }
+  
+  
+}
